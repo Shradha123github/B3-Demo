664e3de664f12e1e8dc65c0ec41b9885755b442d
Shradha123github <jadhavsb29@gmail.com>
2022-10-14 09:11:18 -0700

    Arrayalist program is added

---
+++ ArrayList/src/ArrayListdemo.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayListdemo {
+
+	public static void main(String[] args) {
+
+
+		List<String> list = Arrays.asList("Ram","Sham","Ahana","Saara");
+		
+		System.out.println(list);
+	}
+
+}
